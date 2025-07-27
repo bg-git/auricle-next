f030c4b8204529101f02c7c9d2b1f9569790db84
bg-git <83348874+bg-git@users.noreply.github.com>
2025-07-27 15:38:50 +0100

    feat: use navigation router in auth context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 interface ShopifyCustomer {
   id: string;
